32dc3f734d9ef81b77b4fe6ab7b21e9890450fd1
lilvina <davinaleo2007@yahoo.com>
2019-03-20 15:15:45 -0700

    get functionality works

--- post/postRouter.js
+++ post/postRouter.js
@@ -1,4 +1,29 @@
 const express = require('express')
+const db = require('./postDb.js')
+
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  db.get().then(post => {
+    res.status(200).json(post)
+  }).catch(err => {
+    res.status(400).json({ message: 'The posts could not be retrieved.' })
+  })
+})
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params
+
+  db.getById(id).then(post => {
+    if(post) {
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({ message: 'The post with the specified ID does not exist' })
+    }
+  }).catch(err => {
+    res.status(500).json({ message: 'The post could not be retrieved' })
+  })
+})
+
 
 module.exports = router
--- user/userRouter.js
+++ user/userRouter.js
@@ -1,4 +1,28 @@
 const express = require('express')
+const db = require('./userDb.js')
+
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  db.get().then(user => {
+    res.status(200).json(user)
+  }).catch(err => {
+    res.status(400).json({ message: 'The users could not be retrieved.' })
+  })
+})
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params
+
+  db.getById(id).then(user => {
+    if(user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ message: 'The user with the specified ID does not exist' })
+    }
+  }).catch(err => {
+    res.status(500).json({ message: 'The user could not be retrieved' })
+  })
+})
 
 module.exports = router
