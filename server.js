23cafb6ab0bfcb10687d596824ce72eca28e36fa
lilvina <davinaleo2007@yahoo.com>
2019-03-20 14:39:33 -0700

    working on api challenge day 3

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const helmet = require('helmet')
+
+const server = express()
+const postRouter = require('./post/postRouter.js')
+const userRouter = require('./user/userRouter.js')
+
+server.use(express.json())
+server.use(helmet())
+
+
+server.use('/api/posts', postRouter)
+server.use('/api/users', userRouter)
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h1>Lambda Posts API</h1>
+    <p>Welcome to the Lambda Web API III Challenge</p>`)
+  })
+
+module.exports = server
